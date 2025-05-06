a7ad3c8cbf4f6681060364261402d01d6dc077df
Narayanpurohit <88087099+Narayanpurohit@users.noreply.github.com>
2025-05-06 21:37:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ API_ID = 26847865  # your Telegram API ID
 API_HASH = "0ef9fdd3e5f1ed49d4eb918a07b8e5d6"
 BOT_TOKEN = "7607289349:AAF2HOlsVDzZVZbcd4ICBxu8TMpFtBodnHM"
 
-MONGO_URI = "mongodb://localhost:27017"
+MONGO_URI = "mongodb+srv://gohan1thesaiyan:Q0KU3XQ3KF1ExTNV@cluster0.riwy2lw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 DATABASE_NAME = "filebot"
