66432f0583d62a62f2e47859948fce4218430dc0
Narayanpurohit <88087099+Narayanpurohit@users.noreply.github.com>
2025-05-07 08:42:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 API_ID = 26847865  # your Telegram API ID
 API_HASH = "0ef9fdd3e5f1ed49d4eb918a07b8e5d6"
-BOT_TOKEN = "7607289349:AAF2HOlsVDzZVZbcd4ICBxu8TMpFtBodnHM"
+BOT_TOKEN = "7622470404:AAGSJysCDcVR71897l1mCvuXo16zLaZYihA"
 
-CHANNEL_1_ID = -1001234567890  # Replace with your channel 1 ID
-CHANNEL_2_ID = -1009876543210  # Replace with your channel 2 ID
+CHANNEL_1_ID = -1002032638134  # Replace with your channel 1 ID
+CHANNEL_2_ID = -1002024228864  # Replace with your channel 2 ID
 
 MONGO_URI = "mongodb+srv://gohan1thesaiyan:Q0KU3XQ3KF1ExTNV@cluster0.riwy2lw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 DATABASE_NAME = "filebot"
