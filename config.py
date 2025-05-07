90278a651a7aa75b05036bdc131306c28ca4f178
Narayanpurohit <88087099+Narayanpurohit@users.noreply.github.com>
2025-05-07 18:14:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 API_ID = 26847865  # your Telegram API ID
 API_HASH = "0ef9fdd3e5f1ed49d4eb918a07b8e5d6"
-BOT_TOKEN = "7622470404:AAGSJysCDcVR71897l1mCvuXo16zLaZYihA"
+BOT_TOKEN = "7622470404:AAFrLIDel4CKaqy-UU7lzVNPcNJuyt4cvJA"
 
 CHANNEL_1_ID = -1002032638134  # Replace with your channel 1 ID
 CHANNEL_2_ID = -1002024228864  # Replace with your channel 2 ID
