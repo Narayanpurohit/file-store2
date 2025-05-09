123096046fb456bc37242f9a21af604aef110729
Narayanpurohit <88087099+Narayanpurohit@users.noreply.github.com>
2025-05-09 09:32:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,5 +8,7 @@ CHANNEL_2_ID = -1002024228864  # Replace with your channel 2 ID
 MONGO_URI = "mongodb+srv://gohan1thesaiyan:Q0KU3XQ3KF1ExTNV@cluster0.riwy2lw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 DATABASE_NAME = "filebot"
 
-URL_SHORTENER_API = "https://api.shareus.io/easy_api"
+#URL_SHORTENER_API = "https://api.shareus.io/easy_api"
 URL_SHORTENER_KEY = "zu1xmKAMdlguQSBt5c8ais2aV212"
+URL_SHORTENER_API = "zu1xmKAMdlguQSBt5c8ais2aV212"
+SHORTENER_DOMAIN = "api.shareus.io"
