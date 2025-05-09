447eb5fc63ad385c6146765eab4be23595e7133c
Narayanpurohit <88087099+Narayanpurohit@users.noreply.github.com>
2025-05-09 11:36:16 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,10 +5,10 @@ from datetime import datetime, timedelta
 
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
-from config import API_ID, API_HASH, BOT_TOKEN, URL_SHORTENER_API, SHORTENER_DOMAIN
+from config import API_ID, API_HASH, BOT_TOKEN, URL_SHORTENER_API, SHORTENER_DOMAIN,ADMINS
 from db import files_col, users_col, verifications_col
 
-ADMINS = [123456789, 987654321]  # Replace with actual admin Telegram IDs
+
 
 app = Client("file-bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
